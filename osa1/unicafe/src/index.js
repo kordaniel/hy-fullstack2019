7d11e4c3da94b8db7cc3d0e4e24d9b9fe015e211
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-01-19 17:44:16 +0200

    osa1d exercise unicafe updated to 1.11 osa6 specifications

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,78 +1,63 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
-//import App from './App';
-//import * as serviceWorker from './serviceWorker';
+
+const Statistic = ({text, value}) => (
+  <tr><td>{text}</td><td>{value}</td></tr>
+)
+
+const Statistics = ({good, neutral, bad}) => {
+  const total = (good + neutral + bad)
+  const average = (good - bad) / total
+  const positive = (good / total * 100) + ' %'
+  
+  if (total === 0) {
+    return (
+      <div>
+        <h1>Statistiikka</h1>
+        <p>Ei yhtään palautetta annettu</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h1>Statistiikka</h1>
+      <table>
+        <tbody>
+          <Statistic text={'hyvä'} value={good} />
+          <Statistic text={'neutraali'} value={neutral} />
+          <Statistic text={'huono'} value={bad} />
+          <Statistic text={'yhteensä'} value={total} />
+          <Statistic text={'keskiarvo'} value={average} />
+          <Statistic text={'positiivisia'} value={positive} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [totalClicks, setTotalClicks] = useState(0)
-  const handleGood = () => {
-    setGood(good +1)
-    setAverage(average + 1)
-    setTotalClicks(totalClicks + 1)
-  }
-  const handleNeutral = () => {
-    setNeutral(neutral + 1)
-    setTotalClicks(totalClicks + 1)
-  }
-  const handleBad = () => {
-    setBad(bad + 1)
-    setAverage(average - 1)
-    setTotalClicks(totalClicks + 1)
-  }
-  /*
-  const [clicks, setClicks] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    average: 0,
-    total: 0
-  })
-
-  const handleGood = () => 
-    setClicks({
-      ...clicks,
-      good: clicks.good + 1,
-      average: clicks.average +1,
-      total: clicks.total +1
-    })
-    
   
-  const handleNeutral = () => {
-    const newClicks = {
-      ...clicks,
-      neutral: clicks.neutral + 1,
-      total: clicks.total +1
-    }
-    setClicks(newClicks)
-  }
-  const handleBad = () => {
-    const newClicks = {
-      ...clicks,
-      bad: clicks.bad + 1,
-      average: clicks.average -1,
-      total: clicks.total +1
-    }
-    setClicks(newClicks)
-  }
-  */
+  const handleGood = () => setGood(good +1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
+  
   return (
     <div>
       <h1>Anna palautetta</h1>
-      <button onClick = {handleGood} >hyvä</button>
-      <button onClick = {handleNeutral}>neutraali</button>
-      <button onClick = {handleBad}>huono</button>
-      <h1>Statistiikka</h1>
-      <p>Hyvä {good}</p>
-      <p>Neutraali {neutral}</p>
-      <p>Huono {bad}</p>
-      <p>yhteensä {totalClicks}</p>
-      <p>keskiarvo {average / totalClicks}</p>
-      <p>positiivisia {good / totalClicks * 100} %</p>
+      <Button handleClick={handleGood} text='hyvä' />
+      <Button handleClick={handleNeutral} text='neutraali' />
+      <Button handleClick={handleBad} text='huono' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
@@ -80,8 +65,3 @@ const App = () => {
 ReactDOM.render(<App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-//serviceWorker.unregister();
