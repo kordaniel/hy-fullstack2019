d4330b389a7c3d335496f76c7e10a4688807cbed
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-02-04 00:24:14 +0200

    Osa2 puhelinluettelo part 2.8-2.9 completed

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -6,17 +6,33 @@ import React, { useState } from 'react';
 //const Person = ({name}) => (<div>{name}</div>)
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    {
+      name: 'Arto Hellas',
+      number: '045-123456'
+    },
+    {name: 'Martti Tienari', number: '040-123456'},
+    {name: 'Arto Järvinen', number: '050-123456'},
+    {name: 'Lea Kutvonen', number: '040-654321'}
   ])
   const [newName, setNewName] = useState('Edit to add a new person...')
-  
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const personsToShow = filter === ''
+    ? persons
+    : persons.filter(p => p.name.toLowerCase().includes(filter))
+
   const addPerson = (event) => {
     event.preventDefault()
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
-    //if (persons.includes(person => person.name == newPerson.name)) {
-    //ei toimi, miksi?
+
+    if (newPerson.name === '' || newPerson.number === '') {
+      alert('Täytäthän molemmat kentät')
+      return
+    }
+    
     if (persons.map(p => p.name).includes(newPerson.name)) {
       alert(`${newPerson.name} on jo luettelossa`)
     } else {
@@ -24,29 +40,45 @@ const App = () => {
     }
     
     setNewName('')
+    setNewNumber('')
   }
 
-  const handleFormChange = (event) => (setNewName(event.target.value))
+  const handleFormNameChange = (event) => (setNewName(event.target.value))
+  const handleFormNumberChange = (event) => (setNewNumber(event.target.value))
+  const handleFormFilterChange = (event) => (setFilter(event.target.value.toLowerCase()))
 
-  const rows = () => persons.map(person => 
-    <div key={person.name}>{person.name}</div>
+  const rows = () => personsToShow.map(person => 
+    <div key={person.name}>{person.name} {person.number}</div>
   )
 
   return (
     <div>
       <h2>Puhelinluettelo</h2>
+      <div>
+        rajaa näytettäviä <input
+                            value={filter}
+                            onChange={handleFormFilterChange}
+                          />
+      </div>
+      <h3>Lisää uusi</h3>
       <form onSubmit={addPerson}>
         <div>
           nimi: <input 
                   value={newName}
-                  onChange={handleFormChange}
+                  onChange={handleFormNameChange}
                 />
+        </div>
+        <div>
+          numero: <input 
+                    value={newNumber}
+                    onChange={handleFormNumberChange}
+                  />
         </div>
         <div>
           <button type="submit">lisää</button>
         </div>
       </form>
-      <h2>Numerot</h2>
+      <h3>Numerot</h3>
       {rows()}
     </div>
   )
