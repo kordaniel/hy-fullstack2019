834cd93ed91885f9fb973a462e907c983084dc9d
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-02-03 23:44:02 +0200

    Osa2 part 2.7 implemented and completed

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,7 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import React, { Component } from 'react';
+import React, { useState } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
 
+//const Person = ({name}) => (<div>{name}</div>)
+const App = () => {
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas'}
+  ])
+  const [newName, setNewName] = useState('Edit to add a new person...')
+  
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName
+    }
+    //if (persons.includes(person => person.name == newPerson.name)) {
+    //ei toimi, miksi?
+    if (persons.map(p => p.name).includes(newPerson.name)) {
+      alert(`${newPerson.name} on jo luettelossa`)
+    } else {
+      setPersons(persons.concat(newPerson))
+    }
+    
+    setNewName('')
+  }
+
+  const handleFormChange = (event) => (setNewName(event.target.value))
+
+  const rows = () => persons.map(person => 
+    <div key={person.name}>{person.name}</div>
+  )
+
+  return (
+    <div>
+      <h2>Puhelinluettelo</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          nimi: <input 
+                  value={newName}
+                  onChange={handleFormChange}
+                />
+        </div>
+        <div>
+          <button type="submit">lisää</button>
+        </div>
+      </form>
+      <h2>Numerot</h2>
+      {rows()}
+    </div>
+  )
+}
+/*
 class App extends Component {
   render() {
     return (
@@ -24,5 +74,5 @@ class App extends Component {
     );
   }
 }
-
+*/
 export default App;
