ee88224e8f3f15a934a7f089e5eae13792aa5d57
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-02-04 07:11:26 +0200

    Osa2 2.14 maidentiedot completed. App uses environment variable specified in .env file to hide privat apixu key

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ListLanguages = ({languages}) => (
+const ListLanguages = ({languages}) => 
   languages.map(l => <li key={l.iso639_1}>{l.name}</li>)
+
+const ListCountries = ({selectedCountries, setSelectedCountry}) => (
+  selectedCountries.map(c => {
+    return <div key={c.alpha2Code}>{c.name}
+      <button onClick={() => setSelectedCountry(c.name)}>show</button></div>
+  })
 )
-const Country = ({country}) => {
-  console.log('country', country)
+
+const DisplayCountry = ({country}) => {
   return (
   <div>
     <h1>{country.name}</h1>
@@ -17,45 +23,92 @@ const Country = ({country}) => {
   </div>
   )
 }
-const ShowCountries = ({selectedCountries}) => {
+
+const DisplayWeather = ({weatherData}) => {
+  if (weatherData.length === 0) {
+    return <div></div>
+  }
+  
+  const imageUrl = `https:${weatherData.current.condition.icon}`
+  
+  return (
+    <div>
+      <h3>Weather in {weatherData.location.name}</h3>
+      <div><strong>temperature:</strong> {weatherData.current.temp_c} Celsius</div>
+      <div><img src={imageUrl} alt='' /></div>
+      <div><strong>wind:</strong> {weatherData.current.wind_kph} kph direction {weatherData.current.wind_dir}</div>
+    </div>
+  )
+}
+
+const ShowCountries = ({selectedCountries, setSelectedCountry, updateWeather}) => { 
   if (selectedCountries.length > 10) {
     return (
       <div>Too many matches, specify another filter</div>
     )
   } else if (selectedCountries.length === 1) {
-    console.log('moro')
+    updateWeather()
     return (
-      <Country country={selectedCountries[0]} />
+      <DisplayCountry country={selectedCountries[0]} />
     )
   }
+  /*
+  const currentCountries = selectedCountries.map(c => {
+    return <div key={c.alpha2Code}>{c.name}
+    <button onClick={() => setSelectedCountry(c.name)}>show</button></div>
+  })
+  */
   return (
-    selectedCountries.map(c => <div key={c.alpha2Code}>{c.name}</div>)
+    <ListCountries selectedCountries={selectedCountries}
+                    setSelectedCountry={setSelectedCountry}
+                    />
+
   )
 }
 
 const App = () => {
+  const [weatherData, setWeatherData] = useState([])
   const [countriesData, setCountriesData] = useState([])
   const [selectedCountry, setSelectedCountry] = useState('')
   const selectedCountries = countriesData.filter(
         c => c.name.toLowerCase().startsWith(selectedCountry.toLowerCase()))
+  
   useEffect(() => {
     //console.log('effect')
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(res => setCountriesData(res.data))
   }, [])
-  const handleCountryInput = (e) => (setSelectedCountry(e.target.value))
-  //const handleCountryInput = (e) => (null)
-  //console.log('countries', countriesData)
-  //console.log('selected: ', selectedCountry)
-  console.log('selected countries', selectedCountries)
+  
+  const updateWeather = () => {
+    //what an waste of variables, but oh, this is only a tiny project to learn react
+    const selectedCapital = selectedCountries[0].capital
+    const baseUrl = 'https://api.apixu.com/v1/current.json?key='
+    const apiKey = process.env.REACT_APP_APIXU_KEY
+    //key is assigned in the .env file
+    const endUrl = `&q=${selectedCapital}`
+    const url = `${baseUrl}${apiKey}${endUrl}`
+    useEffect(() => {
+      axios
+        .get(url)
+        .then(res => {
+          setWeatherData(res.data)
+      })
+    }, [])
+    //console.log('lopussa', weatherData)
+  }
+  
+  const handleCountryInput = (e) => setSelectedCountry(e.target.value)
   return (
     <div>
       <div>find countries <input value={selectedCountry}
                             onChange={handleCountryInput}
                       />
       </div>
-      <ShowCountries selectedCountries={selectedCountries} />
+      <ShowCountries selectedCountries={selectedCountries}
+                      setSelectedCountry={setSelectedCountry}
+                      updateWeather={updateWeather} />
+      <DisplayWeather weatherData={weatherData} />
     </div>
   )
 }
