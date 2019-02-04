708811f93cfdb6a04d983f7dde0dd91108b3eb3c
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-02-04 02:29:11 +0200

    Initialized Osa2 2.12* Maiden tiedot

---
+++ osa2/maidentiedot/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  return (
+    <div>
+      <h1>moro</h1>
+    </div>
+  )
+}
+
+export default App;
