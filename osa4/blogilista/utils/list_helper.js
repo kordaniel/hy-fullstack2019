42b68c77ca0c0f35d5d691670130917e37fe6f28
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-02-17 05:39:33 +0200

    Osa4 4.4 Completed, totalLikes listhelper and tests for it implemented, all tests passed

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,15 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = blogs => {
+  return blogs.length === 0
+    ? 0
+    : blogs.map(b => b.likes)
+      .reduce((acc, cur) => acc+cur)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
