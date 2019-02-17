6efabb8f04f73353b9acf73a2c43c2bd57dc5df1
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-02-17 03:24:56 +0200

    Created module blog.js (mongodb connection and schema)

---
+++ osa4/blogilista/models/blog.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+
+const url = config.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('ERROR connecting to MongoDB:', error.message)
+  })
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
