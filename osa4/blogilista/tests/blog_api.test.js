234c0af35fce194e2426a8a2801e537e3f6b5f07
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-02-17 12:02:22 +0200

    Osa4 4.10 added test to check blogs can be sent to /api/blogs with HTTP POST method. (total amount of blogs grows with 1 and the new title is included in all blogs)

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -11,9 +11,33 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-test('there are 2 blogs', async () => {
+test('there are 6 blogs', async () => {
   const response = await api.get('/api/blogs')
-  expect(response.body.length).toBe(2)
+  expect(response.body.length).toBe(6)
+})
+
+test('a valid blog-entry can be added', async () => {
+  const newBlog = {
+    title: 'Blog-Entry by supertest/jest',
+    author: 'Jest Supertest',
+    url: 'http://testing.testing.com/',
+    likes: 0
+  }
+
+  const initialBlogs = await api.get('/api/blogs')
+  const initialAmount = initialBlogs.body.length
+  
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const contents = response.body.map(b => b.title)
+
+  expect(response.body.length).toBe(initialAmount + 1)
+  expect(contents).toContain(newBlog.title)
 })
 
 afterAll(() => {
