d9f93a83d91a48054faedc9d63abd5950dd6ff55
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-02-23 21:48:05 +0200

    Osa5 5.6 completed. Clicking on blog shows all details of the blog

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,30 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+  const showWhenDetailsHidden  = { display: detailsVisible ? 'none' : '' }
+  const showWhenDetailsVisible = { display: detailsVisible ? '' : 'none'}
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle} onClick={() => setDetailsVisible(!detailsVisible)}>
+      <div style={showWhenDetailsHidden}>
+        {blog.title} {blog.author}
+      </div>
+      <div style={showWhenDetailsVisible}>
+        <div>{blog.title} {blog.author}</div>
+        <div><a href={blog.url}>{blog.url}</a></div>
+        <div>{blog.likes} likes <button>like</button></div>
+        <div>Added by {blog.user.name}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
