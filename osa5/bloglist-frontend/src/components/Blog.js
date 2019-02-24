b6f5f277ffc8430f45e3cb446bca65d9d7f8f9f3
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-02-24 04:37:38 +0200

    Osa5 5.15 completed. Added tests for Blog-component. Testing it only renders title and author initially and after clicking it renders all blog details

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -3,7 +3,7 @@ const Blog = ({ blog, increaseBlogLikes, removeBlogHandler, loggedInUsername }) 
   const [detailsVisible, setDetailsVisible] = useState(false)
   const showWhenDetailsHidden  = { display: detailsVisible ? 'none' : '' }
   const showWhenDetailsVisible = { display: detailsVisible ? '' : 'none' }
-  const showWhenUsersOwnBlog = { display: loggedInUsername === blog.user.username ? '' : 'none' }
+  const showWhenUsersOwnBlog = { display: loggedInUsername && loggedInUsername === blog.user.username ? '' : 'none' }
 
   const blogStyle = {
     paddingTop: 10,
@@ -14,15 +14,15 @@ const Blog = ({ blog, increaseBlogLikes, removeBlogHandler, loggedInUsername }) 
   }
 
   return (
-    <div style={blogStyle} onClick={() => setDetailsVisible(!detailsVisible)}>
-      <div style={showWhenDetailsHidden}>
+    <div style={blogStyle} className='blog' onClick={() => setDetailsVisible(!detailsVisible)}>
+      <div style={showWhenDetailsHidden} className='blogTitle'>
         {blog.title} {blog.author}
       </div>
-      <div style={showWhenDetailsVisible}>
+      <div style={showWhenDetailsVisible} className='blogDetails'>
         <div>{blog.title} {blog.author}</div>
         <div><a href={blog.url}>{blog.url}</a></div>
         <div>{blog.likes} likes <button onClick={increaseBlogLikes}>like</button></div>
-        <div>Added by {blog.user.name}</div>
+        <div>Added by {blog.user ? blog.user.name : ''}</div>
         <div><button style={showWhenUsersOwnBlog} onClick={removeBlogHandler}>Remove</button></div>
       </div>
     </div>
