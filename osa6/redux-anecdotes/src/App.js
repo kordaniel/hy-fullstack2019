49ab4ed93c29f804a9081a8d5e63f645fe2c9a1f
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-03-01 13:02:22 +0200

    Osa6 6.3 completed. redux-anecdotes cloned and implemented voting for anecdotes in redux-store

---
+++ osa6/redux-anecdotes/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { voteOnId } from './reducers/anecdoteReducer'
+
+const App = (props) => {
+  const store = props.store
+  const anecdotes = store.getState()
+
+  const vote = (id) => store.dispatch(voteOnId(id))
+  
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form>
+        <div><input /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
