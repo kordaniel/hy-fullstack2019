f23f95f843576dd2ec188c87e8b5111e9da549ab
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-03-02 10:19:47 +0200

    Osa6 6.15 completed. Refactored AnecdoteList component to use only one state prop(s). Filtering anecdotes to show before passing them to the component

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,4 +1,4 @@
-import React        from 'react';
+import React        from 'react'
 import { connect } from 'react-redux'
 import Notification from './components/Notification'
 import AnecdoteForm from './components/AnecdoteForm'
--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -4,25 +4,15 @@ import { voteOnId } from '../reducers/anecdoteReducer'
 import { notificationChange } from '../reducers/notificationReducer'
 
 const AnecdoteList = (props) => {
-  const anecdotes = props.anecdotes
-  const filter = props.filter
-
-  const anecdotesToShow = () => {
-    return filter === '' 
-            ? anecdotes
-            : anecdotes.filter(anecdote => 
-              anecdote.content.toLowerCase().includes(filter.toLowerCase()))
-    }
-
   const vote = id => {
     props.voteOnId(id)
-    const votedAnecdote = anecdotes.find(a => a.id === id).content
+    const votedAnecdote = props.visibleAnecdotes.find(a => a.id === id).content
     props.notificationChange(`You voted '${votedAnecdote}'`)
   }
 
   return (
     <div>
-      {anecdotesToShow().map(anecdote =>
+      {props.visibleAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
@@ -37,12 +27,16 @@ const AnecdoteList = (props) => {
   )
 }
 
+const anecdotesToShow = ({anecdotes, filter}) => {
+  return filter === '' 
+          ? anecdotes
+          : anecdotes.filter(anecdote => 
+            anecdote.content.toLowerCase().includes(filter.toLowerCase()))
+  }
+
 const mapStateToProps = (state) => {
-  //joskus on hyvä tulostella =>
-  //console.log(state)
   return {
-    anecdotes: state.anecdotes,
-    filter: state.filter
+    visibleAnecdotes: anecdotesToShow(state)
   }
 }
 
