a74f2e5985b21b5ed155442738c4275af1c3ce18
Daniel Korpela <42967512+kordaniel@users.noreply.github.com>
2019-12-27 17:44:46 +0200

    Osa8 8.4 completed

--- osa8/backend/library-backend.js
+++ osa8/backend/library-backend.js
@@ -103,7 +103,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -112,7 +112,9 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      return args.author === undefined ? books : books.filter(b => b.author === args.author)
+    },
     allAuthors: () => authors
   },
   Author: {
